152ba073649b646d709d40544c5d53e2798e16a8
dschil138 <davidschiller138@gmail.com>
2023-03-06 01:00:11 -0600

    update config.py to align with Mac menu options

--- config.py
+++ config.py
@@ -3,54 +3,14 @@
 creativity = 0.8
 
 
-
 # The max number of tokens you want to recieve in response. OpenAI charges by the
 # token so this variable is important to keeping your costs down.
 
 max_tokens = 300
 
 
-
-# Which model you would like to send your prompts to.
-# The options, from least powerful and cheap, to most powerful and expensive, are:
-# text-ada-001
-# text-babbage-001
-# text-curie-001
-# text-davinci-003
-# ---
-# code-davinci-002 (this one is for code completions, and is currently free while in beta)
-
-model1 = "text-babbage-001"
-
-model2 = "-davinci-002"
-
-
 # If not manually selecting the prompt, how many lines above the 
 # current cursor position do you want to send as the prompt
 
-lines_to_send = 5
+lines_to_send = 6
 
-
-
-##Hotkey
-# choosing a shortcuts can be a little tricky with this program, so
-# there are currently 3 options to choose from.
-# 1 == Left Alt + Right Ctrl 
-# 2 == Ctrl + 0
-# 3 == Command + Enter
-# 4 == Command + Shift + K
-
-hotkey1 = 4
-
-hotkey2 = 2
-
-
-
-# Prepend - uncomment this variable if you'd like to prepend a string to all your
-# prompts. This can be helpful if you want specific behavior from your completions, for
-# instance, if you want your completions to always have a cheerful demeanor, you
-# could prepend "finish this text the way a cheerful person would:"
-# Keep in mind that you are charged for the words you SEND to the model, as well as the
-# ones you recieve.
-
-universal_prepend = ""
